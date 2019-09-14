2f6b69d4313648c554269693c5d70a3b21c89951
github username <github email address>
2019-09-14 11:30:02 -0400

    trying to submit

--- app/application.rb
+++ app/application.rb
@@ -2,12 +2,11 @@ class Application
     def call(env)
     resp = Rack::Response.new
 
-    time = Time.now.strftime("%H")
-
+    time = Time.now.strftime("%H") 
 
     if time.to_i >= 12
         resp.write "Good Afternoon!"
-    else
+    elsif time.to_i < 12
         resp.write "Good Morning!"
     end
 
